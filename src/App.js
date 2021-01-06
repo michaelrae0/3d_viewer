93a2ba4e34060f0ac11d5a0f7bb2d87ec3c493c1
Michael Rae <mdrae0@gmail.com>
2021-01-05 18:35:45 -0600

    Add base libs and component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import vanillaJS Potree libs, /!\ would be best with proper ES6 import
+const Potree = window.Potree;
+
+export default class PointcloudNavigator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.potreeContainerDiv = React.createRef();
+  }
+
+  render() {
+    return (
+      <div id="potree-root">
+        <div ref={this.potreeContainerDiv} className={"potree_container "}>
+          <div id="potree_render_area"></div>
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    // initialize Potree viewer
+    const viewerElem = this.potreeContainerDiv.current;
+    this.viewer = new Potree.Viewer(viewerElem);
+    this.viewer.setEDLEnabled(true);
+    this.viewer.setFOV(60);
+    this.viewer.setPointBudget(1 * 1000 * 1000);
+    this.viewer.setClipTask(Potree.ClipTask.SHOW_INSIDE);
+    this.viewer.loadSettingsFromURL();
+    this.viewer.setControls(this.viewer.earthControls);
+    // Load and add point cloud to scene
+    let url = "https://raw.githubusercontent.com/potree/potree/develop/pointclouds/lion_takanawa/cloud.js"; // do not forget to put your pointcloud data in the public/pointcloud/ folder
+    Potree.loadPointCloud(url).then(
+      (e) => {
+        let scene = this.viewer.scene;
+        let pointcloud = e.pointcloud;
+        let material = pointcloud.material;
+        material.activeAttributeName = "rgba";
+        material.minSize = 2;
+        material.pointSizeType = Potree.PointSizeType.FIXED;
+        this.viewer.scene.addPointCloud(pointcloud);
+        this.viewer.fitToScreen();
+      },
+      (e) => console.err("ERROR: ", e)
+    );
+  }
+
+  componentDidUpdate() {
+    // If you want to update Potree View/other element upon render(), put it here
+  }
 }
-
-export default App;
